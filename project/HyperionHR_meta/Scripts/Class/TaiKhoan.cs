d01f1d0ecec700fa2afd05985ea5f792dc2b38ac
Dương Hoàng Sâm <samduong.31241026707@st.ueh.edu.vn>
2026-03-03 09:27:36 +0700

    Class5

--- project/HyperionHR_meta/Scripts/Class/TaiKhoan.cs
+++ project/HyperionHR_meta/Scripts/Class/TaiKhoan.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace HyperionHR_meta.Scripts.Class
 {
-    internal class TaiKhoan
+
+
+
+public class TaiKhoan
     {
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public string QuyenHan { get; set; }
+
+        // Constructor mặc định
+        public TaiKhoan()
+        {
+        }
+
+        // Constructor có tham số
+        public TaiKhoan(string tenDangNhap, string matKhau, string quyenHan)
+        {
+            TenDangNhap = tenDangNhap;
+            MatKhau = matKhau;
+            QuyenHan = quyenHan;
+        }
     }
 }
+
