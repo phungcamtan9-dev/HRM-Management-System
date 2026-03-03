e998de42ded775c5fda74a32ad4db6ce9342d5c6
Dawnn <phungcamtan9@gmail.com>
2026-03-03 13:41:41 +0700

    CodeN1

---
+++ project/HyperionHR_meta/Scripts/Class/People/NhanVienFullTime.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperionHR_meta.Scripts.Class.People
+{
+    public class NhanVienFullTime : NhanVien
+    {
+        private double luongCoBan;
+        private double phuCap;
+
+        public double LuongCoBan
+        {
+            get { return luongCoBan; }
+            set { luongCoBan = value; }
+        }
+
+        public double PhuCap
+        {
+            get { return phuCap; }
+            set { phuCap = value; }
+        }
+
+        public NhanVienFullTime() { }
+
+        public NhanVienFullTime(string maNV, string hoTen, string gioiTinh, DateTime ngaySinh,
+                                string sdt, string email, string diaChi,
+                                string queQuan, string chucVu, string phongBan,
+                                double luongCoBan, double phuCap)
+            : base(maNV, hoTen, gioiTinh, ngaySinh, sdt, email, diaChi, queQuan, chucVu, phongBan)
+        {
+            this.luongCoBan = luongCoBan;
+            this.phuCap = phuCap;
+        }
+
+        
+    }
+}
---
+++ project/HyperionHR_meta/Scripts/Class/People/NhanVienPartTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperionHR_meta.Scripts.Class.People
+{
+    public class NhanVienPartTime : NhanVien
+    {
+        private double luongTheoGio;
+        private double tongGioLam;
+
+        public double LuongTheoGio
+        {
+            get { return luongTheoGio; }
+            set { luongTheoGio = value; }
+        }
+
+        public double TongGioLam
+        {
+            get { return tongGioLam; }
+            set { tongGioLam = value; }
+        }
+
+        public NhanVienPartTime() { }
+
+        public NhanVienPartTime(string maNV, string hoTen, string gioiTinh, DateTime ngaySinh,
+                                string sdt, string email, string diaChi,
+                                string queQuan, string chucVu, string phongBan,
+                                double luongTheoGio, double tongGioLam)
+            : base(maNV, hoTen, gioiTinh, ngaySinh, sdt, email, diaChi, queQuan, chucVu, phongBan)
+        {
+            this.luongTheoGio = luongTheoGio;
+            this.tongGioLam = tongGioLam;
+        }
+
+        
+    }
+}
