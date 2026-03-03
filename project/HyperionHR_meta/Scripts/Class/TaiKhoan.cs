21c58a4802963175cd1a974065b81146e208bc36
Dương Hoàng Sâm <samduong.31241026707@st.ueh.edu.vn>
2026-03-03 16:15:06 +0700

    Update TaiKhoan.cs

--- project/HyperionHR_meta/Scripts/Class/TaiKhoan.cs
+++ project/HyperionHR_meta/Scripts/Class/TaiKhoan.cs
@@ -6,26 +6,31 @@ using System.Threading.Tasks;
 
 namespace HyperionHR_meta.Scripts.Class
 {
-
-
-
-public class TaiKhoan
+    public class TaiKhoan
     {
+        public string MaTaiKhoan { get; set; }
         public string TenDangNhap { get; set; }
         public string MatKhau { get; set; }
         public string QuyenHan { get; set; }
+        public bool TrangThai { get; set; }
+        public DateTime NgayTao { get; set; }
 
-        // Constructor mặc định
+        // Constructor không tham số
         public TaiKhoan()
         {
+            NgayTao = DateTime.Now;
+            TrangThai = true;
         }
 
         // Constructor có tham số
-        public TaiKhoan(string tenDangNhap, string matKhau, string quyenHan)
+        public TaiKhoan(string maTaiKhoan, string tenDangNhap, string matKhau, string quyenHan, bool trangThai)
         {
+            MaTaiKhoan = maTaiKhoan;
             TenDangNhap = tenDangNhap;
             MatKhau = matKhau;
             QuyenHan = quyenHan;
+            TrangThai = trangThai;
+            NgayTao = DateTime.Now;
         }
     }
 }
