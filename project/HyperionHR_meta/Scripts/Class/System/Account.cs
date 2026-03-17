304e846f5b3eb2531c010c3a105618cecdf26d25
Dương Hoàng Sâm <samduong.31241026707@st.ueh.edu.vn>
2026-03-17 14:19:47 +0700

    Update Account

--- project/HyperionHR_meta/Scripts/Class/System/Account.cs
+++ project/HyperionHR_meta/Scripts/Class/System/Account.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using HyperionHR_meta.Scripts.Class.Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperionHR_meta.Scripts.Class.System
+{
+    [Serializable]
+    public class Account
+    {
+        //thuộc tính
+        public string Username { get; set; }
+        public string Password { get; private set; }
+        public Role Role { get; set; } //biến để phân quyền
+
+        // liên kết với Employee
+        public Employee Employee { get; set; }
+
+        //Constructor
+        public Account(string userName, string password, Role role)
+        {
+            this.Username = userName;
+            this.Password = password;
+            this.Role = role;
+        }
+
+        //Methods:
+
+    }
+}
