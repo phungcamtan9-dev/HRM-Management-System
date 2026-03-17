cd0fabc705efdd2a3cbec9bc0ff92614220adb5d
Long Pham <longpham.31241021371@st.ueh.edu.vn>
2026-03-17 13:34:23 +0700

    Update Person

--- project/HyperionHR_meta/Scripts/Class/Person/PartTimeEmployee.cs
+++ project/HyperionHR_meta/Scripts/Class/Person/PartTimeEmployee.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using HyperionHR_meta.Scripts.Class.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyperionHR_meta.Scripts.Class.Person;
+using System.Diagnostics.Contracts;
+
+namespace HyperionHR_meta.Scripts.Class.Person
+{
+    [Serializable]
+    public class PartTimeEmployee : Employee
+    {
+        public double SoGioLam { get; set; }
+        public double HeSoLuongGio { get; set; }
+
+        public PartTimeEmployee()
+        {
+        }
+
+        public PartTimeEmployee(string id, string hoTen, DateTime ngaySinh, string email, string soDienThoai,
+                                string maNhanVien, Department phongBan, Position chucVu,
+                                double soGioLam, double heSoLuongGio)
+            : base(id, hoTen, ngaySinh, email, soDienThoai, maNhanVien, phongBan, chucVu)
+        {
+            SoGioLam = soGioLam;
+            HeSoLuongGio = heSoLuongGio;
+        }
+    }
+}
